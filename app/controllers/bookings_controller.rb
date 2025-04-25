8ec7021a59c68126d52eefc01eccff32c5a57cdc
Emanuel Flores <flores.emanuel.108@gmail.com>
2025-04-24 22:17:17 -0400

    polished controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
   def new
+    @videogame = Videogame.find(params[:videogame_id])
+    @booking = @videogame.bookings.new
   end
 
   def create
+    @videogame = Videogame.find(params[:videogame_id])
+    @booking = @videogame.bookings.build(booking_params)
+    if @booking.save
+      redirect_to videogame_path(@booking.videogame)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to videogame_path(@booking.videogame), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,7 @@
 class Booking < ApplicationRecord
-
   belongs_to :user
   belongs_to :videogame
 
   validates :start_time, :end_time, presence: true
   validate :end_after_start
-
 end
