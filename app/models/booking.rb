f62678402a58ab1b390aef4d56657a96b4b66020
Alexis Giard <alexis.giard96@gmail.com>
2025-04-24 21:34:05 -0400

    Modele completed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,9 @@
 class Booking < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :videogame
+
+  validates :start_time, :end_time, presence: true
+  validate :end_after_start
+
 end
--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -1,2 +1,6 @@
 class Videogame < ApplicationRecord
+
+  has_many :bookings
+
+  validates :name, :description, :rating, :price, presence: true
 end
